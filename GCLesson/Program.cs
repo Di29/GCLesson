0cc6fb3fe16425ddcde16bbc716947727cde738a
Urinbasarov Dias <jentle.men29@gmail.com>
2018-10-12 18:26:25 +0600

    done

---
+++ GCLesson/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GCLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> list = new List<object>();
+
+            for(int i = 0; i<50; i++)
+            {
+                list.Add(new { Id = 1, Name = "admin" });
+                Thread.Sleep(500);
+            }
+            list = null;
+            GC.Collect();
+        }
+    }
+}
---
+++ GCLesson/SomeFilesCreator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GCLesson
+{
+    public class SomeFilesCreator : IDisposable
+    {
+        //какие-то методы для работы с файлами
+
+        public void Open()
+        {
+            //реализация открытия потоков, подключения и т.д.
+        }
+
+        public void Close()
+        {
+            //реализация закрытия потоков, подключения и т.д.
+        }
+
+        public void Dispose()
+        {
+            //очистка неуправляемых ресурсов
+        }
+    }
+}
